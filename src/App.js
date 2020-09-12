4af12abb6d476f2280bde6e06263d3b9291784a4
Amrit-Raj12 <amrit.raj1224@gmail.com>
2020-09-12 12:22:21 +0530

    NetArt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="header">
+        <center>
+          <img src="asserts/logo.png" alt="" />
+        </center>
+      </nav>
+      <div className="container">
+      <dl>
+
+        <dt>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</dt>
+            <dd>&#8226; C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for <br /> various projects across the globe to save energy.</dd>
+            <dd>&#8226; C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5
+                Star rated energy efficient smart pumps with IoT enabled control panel. </dd>
+
+       </dl>
+       </div>
+       <div className="row">
+      <div className="container_center">
+       <img className="" src="asserts/1.png" style={{width:"350px"}} />
+        <img className="responsive" src="asserts/2.png" style={{width:"600px"}} />
+      </div>
+      </div>
+      <div className="container">
+      <dl>
+      <dd>Government of India has awarded the <span style={{fontWeight:'bold'}}> "National Energy Conservation Award 2018"</span>. Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri.
+      Raj Kumar Singh, Honorable Minister of State.</dd>
+      </dl>
+
+      </div>
+      <p>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING
+      OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION. </p>
+
+      <div className="banner">
+        <img src="asserts/3.png" alt="" style={{width:"1200px"}}  />
+      </div>
+        <p className="para">Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors</p>
+        <hr style={{color:'#BA3235'}} />
+          
+        <h3>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</h3>
+
+        <hr style={{color:'#BA3235'}} />
+
+        <p className="para">CHEMICALS & PROCESS<span style={{color:'#BA3235'}}> | </span> POWER<span style={{color:'#BA3235'}}> | </span> WATER & WASTE WATER<span style={{color:'#BA3235'}}> | </span> OILS & GAS<span style={{color:'#BA3235'}}> | </span> PHARMA<span style={{color:'#BA3235'}}> | </span> SUGARS & DISTILLERIES<span style={{color:'#BA3235'}}> | </span> PAPER & PULP<span style={{color:'#BA3235'}}> | </span> MARINE & DEFENCE<span style={{color:'#BA3235'}}> | </span> METAL & MINING<span style={{color:'#BA3235'}}> | </span> FOOD & BEVERAGE<span style={{color:'#BA3235'}}> | </span> PETROCHEMICAL & REFINERIES<span style={{color:'#BA3235'}}> | </span> SOLAR<span style={{color:'#BA3235'}}> | </span> BUILDING<span style={{color:'#BA3235'}}> | </span> HVAC<span style={{color:'#BA3235'}}> | </span> FIRE FIGHTING <span style={{color:'#BA3235'}}> | </span>AGRICULTURE & RESIDENTIAL</p>
+
+        <div class="footer">
+  <ul>
+    <li><a href=""><i class="fa fa-phone" /><span> </span>Toll free 1800 200 1234</a></li>
+    <li><a href=""><i class="fa fa-facebook-square" /><span> </span>www.facebook.com/cripumps</a></li>
+    <li><a href=""><i class="fa fa-globe" /><span> </span>www.crigroups.com</a></li>
+
+  </ul>
+</div>
+
     </div>
+
   );
 }
 
